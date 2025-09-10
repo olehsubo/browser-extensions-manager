86be7605f21d3d4ad58a3691d6efd411e0bbaf82
Oleh Subotin <230963052+olehsubo@users.noreply.github.com>
2025-09-10 10:39:30 +0300

    style: filter btns

--- src/components/FilterButtons.tsx
+++ src/components/FilterButtons.tsx
@@ -5,18 +5,18 @@ const FilterButtons = () => {
   const [activeBtn, setActiveBtn] = useState('All');
 
   return (
-    <div className='flex gap-2'>
+    <div className='flex gap-3'>
       {filters.map((filter) => (
         <button
           key={filter}
           type='button'
           aria-pressed={activeBtn === filter}
           onClick={() => setActiveBtn(filter)}
-          className={`px-5 py-2 text-md font-semibold rounded-3xl focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-neutral-900
+          className={`px-5 py-2 text-md font-semibold rounded-3xl  focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-neutral-900
             ${
               activeBtn === filter
                 ? ' bg-red-500 text-neutral-900'
-                : 'bg-neutral-700 text-neutral-0 hover:bg-neutral-600 '
+                : 'bg-neutral-700 text-neutral-0 ring-2 ring-neutral-300 hover:bg-neutral-600 '
             }
           `}
         >
